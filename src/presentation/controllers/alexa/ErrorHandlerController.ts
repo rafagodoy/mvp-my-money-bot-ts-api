2c044f8469c775ac252b40683d1015c8686428f1
Rafael Godoy <gdiniz.ti@gmail.com>
2022-12-12 09:14:10 -0300

    feat: create controllers to alexa skills
    - to welcome intent
    
    - to session ended
    
    - to error handling
    
    - to lauch request

---
+++ src/presentation/controllers/alexa/ErrorHandlerController.ts
@@ -0,0 +1,22 @@
+import { AlexaSkillSDK } from '@/adapters/voice-skills/protocols';
+import { 
+  AlexaRequest,
+  AlexaResponse,
+  AlexaVoiceController,
+} from '@/presentation/protocols';
+
+export class ErrorHandlerController implements AlexaVoiceController {
+
+  constructor(
+    private readonly sdk: AlexaSkillSDK,
+  ) {}
+
+  async canHandle(): Promise<boolean> {
+    return true;
+  }
+
+  async handle(input: AlexaRequest, error): AlexaResponse {
+    console.log(`~~~~ Error handled: ${error.stack}`);
+    return this.sdk.response(input, '', error);
+  }
+}
---
+++ src/presentation/controllers/alexa/LaunchRequestController.ts
@@ -0,0 +1,30 @@
+import { AlexaSkillSDK } from '@/adapters/voice-skills/protocols';
+import { Translator } from '@/domain/interactions';
+import { 
+  AlexaRequest,
+  AlexaResponse,
+  RequestType,
+  AlexaVoiceController,
+} from '@/presentation/protocols';
+
+export class LaunchRequestController implements AlexaVoiceController {
+
+  constructor(
+    private readonly sdk: AlexaSkillSDK,
+    private readonly translator: Translator,
+    private readonly requestType: RequestType = 'LaunchRequest',
+  ) {}
+
+  async canHandle(input: AlexaRequest): Promise<boolean> {
+    const intentName = await this.sdk.getIntentName(input);
+
+    return this.sdk.request(input, intentName, this.requestType);
+  }
+
+  async handle(input: AlexaRequest): AlexaResponse {
+    const intentName = await this.sdk.getIntentName(input);
+    const speechOutput = this.translator.byIntentName(intentName);
+
+    return this.sdk.response(input, speechOutput);
+  }
+}
