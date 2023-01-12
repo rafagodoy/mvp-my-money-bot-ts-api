b42b67886efcaee728759615380d604692272a89
Rafael Godoy <gdiniz.ti@gmail.com>
2023-01-12 07:49:05 -0300

    feature: add alexa skill error handler

--- src/main/handlers/alexa/index.ts
+++ src/main/handlers/alexa/index.ts
@@ -1,10 +1,12 @@
 import { SkillBuilders } from 'ask-sdk-core';
 import { welcomeIntentController } from '../../factories/alexa/welcome-intent.factory';
 import { launchRequestController } from '../../factories/alexa/launch-request.factory';
+import { errorHandlerController } from '../../factories/alexa/error-handler.factory';
 
 exports.handlers = SkillBuilders.custom()
   .addRequestHandlers(
-    launchRequestController,
     welcomeIntentController,
+    launchRequestController,
   )
+  .addErrorHandlers(errorHandlerController)
   .lambda();
