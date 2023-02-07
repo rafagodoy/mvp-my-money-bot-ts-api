7a88f4bf411b04a01d55e5cbd2567e2ef6da2546
Rafael Godoy <gdiniz.ti@gmail.com>
2023-02-07 12:51:35 -0300

    chore: get .env vars and share it in the project

--- src/main/handlers/api/index.ts
+++ src/main/handlers/api/index.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { AWSLambdaAdapter } from '../../aws-lambda.adapter';
 import { makeGetStockPriceController } from '../../factories/api/get-stock-price.factory';
 
