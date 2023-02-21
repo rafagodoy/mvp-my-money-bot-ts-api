d456906c3e577c6699a7ba79abe5eabf364a5482
Rafael Godoy <gdiniz.ti@gmail.com>
2023-02-20 22:34:24 -0300

    feat: create /stocks endpoint
    - return stocks names
    
    - find stock names by companyName query parameter

--- src/main/handlers/api/index.ts
+++ src/main/handlers/api/index.ts
@@ -3,10 +3,20 @@ dotenv.config();
 
 import { AWSLambdaAdapter } from '../../aws-lambda.adapter';
 import { makeGetStockPriceController } from '../../factories/api/get-stock-price.factory';
+import { makeGetStockNameController } from '../../factories/api/get-stock-name.factory';
 
 module.exports.getStockPrice = async (event) => {
 
   const controller = makeGetStockPriceController();
+  const awsLambda = new AWSLambdaAdapter(controller);
+
+  return awsLambda.start(event);
+  
+};
+
+module.exports.getStockName = async (event) => {
+
+  const controller = makeGetStockNameController();
   const awsLambda = new AWSLambdaAdapter(controller);
 
   return awsLambda.start(event);
