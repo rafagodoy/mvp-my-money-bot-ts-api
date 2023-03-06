8ae7b5ffca91bfe9f87054c8df41790312d6c34c
Rafael Godoy <gdiniz.ti@gmail.com>
2023-03-06 12:49:54 -0300

    refactor: change alexa sessionEnded factory

--- src/main/factories/alexa/session-ended-request.factory.ts
+++ src/main/factories/alexa/session-ended-request.factory.ts
@@ -1,17 +1,12 @@
-import { TranslatorAdapter } from '@/adapters/voice-skills/translator.adapter';
 import { AlexaSkills } from '@/infra/libs/alexa-skills';
-import { Speaker } from '@/adapters/interactions/speaker';
 import {
   SessionEndedRequestController,
 } from '@/presentation/controllers/alexa/SessionEndedRequestController';
 
-const speaker = new Speaker();
 const sdk = new AlexaSkills();
-const translator = new TranslatorAdapter(speaker);
 
 const sessionEndedRequestController = new SessionEndedRequestController(
   sdk,
-  translator,
 );
 
 export { sessionEndedRequestController };
