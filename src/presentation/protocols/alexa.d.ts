3a397437b20ee9b70ac4d37f7530a77241fb685d
Rafael Godoy <gdiniz.ti@gmail.com>
2023-03-06 12:50:30 -0300

    chore: add IntentRequest protocol from Alexa

--- src/presentation/protocols/alexa.d.ts
+++ src/presentation/protocols/alexa.d.ts
@@ -1,6 +1,7 @@
 import { HandlerInput } from 'ask-sdk-core';
-import { Response } from 'ask-sdk-model';
+import { Response, IntentRequest } from 'ask-sdk-model';
 
 export type AlexaRequest = HandlerInput;
 export type AlexaResponse = Promise<Response>;
 export type RequestType = 'LaunchRequest' | 'IntentRequest' | 'SessionEndedRequest';
+export type AlexaIntent = IntentRequest;
