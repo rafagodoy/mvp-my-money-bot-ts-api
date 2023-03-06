e0addd02c0614af693ca5e5f00211bd351fc1bfc
Rafael Godoy <gdiniz.ti@gmail.com>
2023-03-06 12:49:14 -0300

    refactor: transform speechText param like optional

--- src/adapters/voice-skills/protocols/alexa-skills.d.ts
+++ src/adapters/voice-skills/protocols/alexa-skills.d.ts
@@ -1,7 +1,6 @@
 import { AlexaRequest, AlexaResponse, RequestType } from '@/presentation/protocols';
 
 export interface AlexaSkillSDK {
-  getIntentName(input: AlexaRequest): Promise<string>,
   request(input: AlexaRequest, intentName: string, requestType: RequestType): Promise<boolean>,
-  response(input: AlexaRequest, speechText: string, error?: any): AlexaResponse
+  response(input: AlexaRequest, speechText?: string, error?: any): AlexaResponse
 }
