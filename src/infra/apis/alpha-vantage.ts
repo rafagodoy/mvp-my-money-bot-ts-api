25475f072edba3c6ff1070b46f2a954e78e5e6d0
Rafael Godoy <gdiniz.ti@gmail.com>
2023-06-02 09:00:47 -0300

    chore: throw errors from alphaVantage API

--- src/infra/apis/alpha-vantage.ts
+++ src/infra/apis/alpha-vantage.ts
@@ -8,48 +8,72 @@ import {
   GetStockPriceAPIResponse,
 } from '@/adapters/apis/protocols';
 
+import { GatewayTimeoutError, BadGatewayError } from '@/presentation/errors';
+
 export class AlphaVantage implements AlphaVantageAPI {
   private readonly urlAPI = alphaVantage.url;
 
   private readonly apiSecret = alphaVantage.apiSecret;
 
+  private readonly resourceName = 'AlphaVantageAPI';
+
   constructor(
     private readonly nodeFetch: API,
   ) {}
 
-  async getStockPrice(settings: GetStockPriceAPIRequest): Promise<GetStockPriceAPIResponse> {
-    const response = await this.nodeFetch.get({
-      url: this.urlAPI,
-      pathParams: '/query',
-      queryParams: [
-        ...settings.queryParams,
-        {
-          function: 'TIME_SERIES_DAILY_ADJUSTED',
-        },
-        {
-          apikey: this.apiSecret,
-        },
-      ],
-    });
+  private throwError(error) {
+    
+    const detais = JSON.stringify(error.details);
+    
+    if (error.status === 504) {
+      throw new GatewayTimeoutError(this.resourceName, detais);
+    }
 
-    return response.body as GetStockPriceAPIResponse;
+    throw new BadGatewayError(this.resourceName, detais);
+  }
+
+  async getStockPrice(settings: GetStockPriceAPIRequest) {
+    try {
+
+      const response = await this.nodeFetch.get({
+        url: this.urlAPI,
+        pathParams: '/query',
+        queryParams: [
+          ...settings.queryParams,
+          {
+            function: 'TIME_SERIES_DAILY_ADJUSTED',
+          },
+          {
+            apikey: this.apiSecret,
+          },
+        ],
+      });
+
+      return response.body as GetStockPriceAPIResponse;
+    } catch (error) {
+      this.throwError(error);
+    }
   }
 
   async getStockName(settings: GetStockNameAPIRequest): Promise<GetStockNameAPIResponse> {
-    const response = await this.nodeFetch.get({
-      url: this.urlAPI,
-      pathParams: '/query',
-      queryParams: [
-        ...settings.queryParams,
-        {
-          function: 'SYMBOL_SEARCH',
-        },
-        {
-          apikey: this.apiSecret,
-        },
-      ],
-    });
+    try {
+      const response = await this.nodeFetch.get({
+        url: this.urlAPI,
+        pathParams: '/query',
+        queryParams: [
+          ...settings.queryParams,
+          {
+            function: 'SYMBOL_SEARCH',
+          },
+          {
+            apikey: this.apiSecret,
+          },
+        ],
+      });
 
-    return response.body as GetStockNameAPIResponse;
+      return response.body as GetStockNameAPIResponse;
+    } catch (error) {
+      this.throwError(error);
+    }
   }
 }
